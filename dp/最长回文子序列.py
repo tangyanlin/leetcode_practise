ee00fa68ba052b810d62ed81460bad525765b04d
Tang Yanlin <tangyanlin0210@163.com>
2023-10-15 20:43:30 +0800

    Create 最长回文子序列.py

---
+++ dp/最长回文子序列.py
@@ -0,0 +1,20 @@
+class Solution:
+    def longestPalindromeSubseq(self, s: str) -> int:
+        dp = []
+        for i in range(len(s)):
+            temp = []
+            for j in range(len(s)):
+                temp.append(0)
+            dp.append(temp)
+        
+        for i in range(0, len(s)):
+            dp[i][i] = 1
+        
+        for i in range(len(s)-2, -1, -1):
+            for j in range(i+1, len(s)):
+                if s[i] == s[j]:
+                    dp[i][j] = dp[i+1][j-1] +2
+                else:
+                    dp[i][j] = max(dp[i+1][j], dp[i][j-1])
+        
+        return dp[0][len(s)-1]

227ea6d4d9d2592a078906c7ec29b056a62259f8
tangyanlin <tangyanlin@360.cn>
2023-12-18 20:08:34 +0800

    add

--- array/sub_array.py
+++ array/sub_array.py
@@ -1,20 +1,19 @@
 #encoding=utf-8
 
 def array_sub(nums=[1,2,5,2], k =11):
-    i = 0
-    j = 0
-    n = len(nums)
-    products = 1
-    res = 0
-    while i < n and j <= n:
-        if products < k and j < n:
-            products = products * nums[j]
-            res += 1
-            j += 1
-        else:
-            products = products / nums[i]
-            res += 1
-            i += 1
-    return res
+    if k <= 1:
+        return 0
+    L = 0
+    R = 0
+    count = 0
+    base = 1
+    while R < len(nums):
+        base *= nums[R]
+        while base >= k:
+            base /= nums[L]
+            L += 1
+        count += (R - L + 1)
+        R += 1
+    return count
 
 print(array_sub())

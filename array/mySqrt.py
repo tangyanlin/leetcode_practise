20c8916c08881db647f6df08acd943edf7ca2d98
tangyanlin <tangyanlin@360.cn>
2023-12-22 15:53:49 +0800

    add

---
+++ array/mySqrt.py
@@ -0,0 +1,16 @@
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        if x == 0:
+            return 0
+        if x == 1:
+            return 1
+        left = 0
+        right = int(x/2)+1
+        while left < right:
+            middle = left + int((right -left+1)/2)
+            res = middle*middle
+            if res <= x:
+                left = middle
+            else:
+                right = middle - 1
+        return left
